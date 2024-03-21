65201584c45ee9415948b7be26c85bfe0c9c5d8d
xStranged1 <fede.valle04@gmail.com>
2024-03-21 00:10:24 -0300

    refactor:

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,23 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { supabase } from "./utils/supabase"
 
 
-function Home(){
+function Home({session}){
+
+  useEffect(() => {
+    console.log('use home');
+    async function getUsers() {
+      const { users } = await supabase.schema("auth").from("users").select();
+      
+    }
+    console.log(session);
+    
+    
+  },[])
     
   return(
       <div>
-          <h1>Hola! {usuario} </h1>
-          <button onClick={() => supabase.auth.signOut().then((res)=>console.log(res))}>Sign out</button>
+          <h1>Hola! {session.user.user_metadata.full_name} </h1>
+          <button onClick={() => supabase.auth.signOut()}>Sign out</button>
       </div>
   )
 }
@@ -20,7 +31,7 @@ function Home(){
 
 export default function App() {
   const [session, setSession] = useState(null)
-  const [user, setUser] = useState(null)
+  const [users, setUsers] = useState([])
 
 
   useEffect(() => {
@@ -40,6 +51,6 @@ export default function App() {
     return (<Auth supabaseClient={supabase} providers={['google', 'facebook']} appearance={{ theme: ThemeSupa }} />)
   }
   else {
-    return (<Home />)
+    return (<Home session = {session} />)
   }
 }
